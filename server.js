11ed335dccfea087cab1912c2293270e75a7bdce
Vipu96 <114683817+Vipu96@users.noreply.github.com>
2025-10-07 20:06:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import cors from "cors";
-app.use(cors());
 import express from "express";
+import cors from "cors";
 import fs from "fs";
 import crypto from "crypto";
 import fetch from "node-fetch";
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // Environment variables (add in Railway)
